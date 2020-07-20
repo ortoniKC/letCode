19fbc6aac3b2fe73f3cafc198503ea59ca79a13e
Koushik Chatterjee <58769833+ortoniKC@users.noreply.github.com>
2020-07-20 15:59:40 +0530

    https://youtu.be/zAspD6KO3DU
    Find the video:
    https://youtu.be/zAspD6KO3DU

--- zipFileOrFolder/ZipFiles.java
+++ zipFileOrFolder/ZipFiles.java
@@ -3,6 +3,7 @@ package zipFileOrFolder;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
@@ -10,23 +11,21 @@ import java.util.Base64;
 import org.openqa.selenium.io.Zip;
 
 public class ZipFiles {
-	static BufferedOutputStream outputStream;
-	public static void main(String[] args) {
-		try {
-			Zip.unzip(new FileInputStream("Downloads.zip"), 
-					new File("./files"));
-		} catch (IOException e) { e.printStackTrace(); }
-		try {
-			File file = new File("./files");
-			String zip = Zip.zip(file);
-			//UPDATE
-			byte[] decode = Base64.getDecoder().decode(zip);
-			outputStream = new BufferedOutputStream(
-					new FileOutputStream("./zipping.zip"), 10000);
-			outputStream.write(decode);
-		} catch (IOException e) {e.printStackTrace();}
-		finally {
-			try {outputStream.close();} catch (IOException e) {e.printStackTrace();}
-		}
+
+	public static void main(String[] args) throws IOException {
+		String zip = Zip.zip(new File("./images"));
+
+		BufferedOutputStream stream = new BufferedOutputStream(
+			new FileOutputStream("./images.zip"), 10000);
+		byte[] decode = Base64.getDecoder().decode(zip);
+		stream.write(decode);
+		stream.close();
 	}
+
+	// public static void main(String[] args) throws FileNotFoundException,
+	// IOException {
+
+	// Zip.unzip(new FileInputStream("images.zip"), new File("./images"));
+
+	// }
 }
